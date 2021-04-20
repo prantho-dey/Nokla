23c6bad2a6b1730d01ca0b39edd8902f9335fc7e
Prantho <pranthodey89@gmail.com>
2021-04-21 00:47:58 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+(function ($) {
+
+    'use strict';
+
+	/*------------------------------------
+		Mobile Menu
+	--------------------------------------*/
+
+	/*-------------------------------------------
+	    Sticky Header
+	--------------------------------------------- */
+
+	let win = $(window);
+	let sticky_id = $(".header-area");
+	win.on('scroll', function () {
+		let scroll = win.scrollTop();
+		if (scroll < 245) {
+			sticky_id.removeClass("sticky-header");
+		} else {
+			sticky_id.addClass("sticky-header");
+		}
+	});
+
+
+	/*------------------------------------
+        Overlay Close
+	--------------------------------------*/
+	$(window).scroll(function () {
+		if ($(this).scrollTop() !== 0) {
+			$('#scrollUp').fadeIn();
+		} else {
+			$('#scrollUp').fadeOut();
+		}
+    });
+    
+	$('#scrollUp').on('click', function () {
+		$("html, body").animate({scrollTop: 0}, 600);
+		return false;
+	});
+
+
+	/*------------------------------------
+	Odometer Counter
+	--------------------------------------*/
+	$('.odometer').appear(function (e) {
+		var odo = $(".odometer");
+		odo.each(function () {
+			var countNumber = $(this).attr("data-count");
+			$(this).html(countNumber);
+		});
+	});
+
+	/* magnificPopup img view */
+	$('.popup-image').magnificPopup({
+		type: 'image',
+		gallery: {
+		enabled: true
+		}
+	});
+
+	/* Meet video view */
+	$('.meet-popup').magnificPopup({
+		type: 'iframe'
+	});
+
+
+})(jQuery);
